4c6b4554ea974cb308e9769eb3cd63b8874c9b2e
Aaron K <tonberryspawn@gmail.com>
2022-03-27 11:46:31 -0700

    more notes

--- Ruby-Rails/1_ruby_notes/3_comparison_operators.rb
+++ Ruby-Rails/1_ruby_notes/3_comparison_operators.rb
@@ -1,3 +1,11 @@
-# Comparison opoerators
+# Comparison operators
 
+puts 10 == 10         # => true
+puts 10 == 9          # => false
+puts 10 == "10"       # => false
+puts 10 == "10".to_i  # => true
+puts 10 == "10".to_f  # => true
+puts 10 === 10.0      # => true
+puts 10.eql?(10.0)    # => false .eql compares types
+puts 10 != 9          # => true
 
