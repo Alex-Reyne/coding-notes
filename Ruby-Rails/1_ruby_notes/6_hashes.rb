c0b9d2722151a53d0eb1166bb680e87280c5461b
Aaron K <tonberryspawn@gmail.com>
2022-03-27 23:36:38 -0700

    hashes complete

--- Ruby-Rails/1_ruby_notes/6_hashes.rb
+++ Ruby-Rails/1_ruby_notes/6_hashes.rb
@@ -11,5 +11,10 @@ puts
 # Hash w/ symbols
 p symbol_hash = {a: 1, b: 2, c: 3}
 p symbol_hash[:a] # => 1
-
-
+symbol_hash[:d] = 4
+p symbol_hash     # => {:a=>1, :b=>2, :c=>3, :d=>4}
+symbol_hash[:a] = 'new val'
+p symbol_hash     # => {:a=>"new val", :b=>2, :c=>3, :d=>4}
+symbol_hash.each {|key, value| puts "The key is #{key} and the value is #{value}"}
+p symbol_hash.select {|k, v| v.is_a?(String) } # => {:a=>"new val"}
+p symbol_hash.each {|k, v| symbol_hash.delete(k) if v.is_a?(String)} # => {:b=>2, :c=>3, :d=>4}
